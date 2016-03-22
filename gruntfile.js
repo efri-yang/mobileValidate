18363e991e4dc6413c8c55078a0fda20d01143ef
efri-yang <948061564@qq.com>
2016-03-23 01:13:29 +0800

    将mvalidate.js的zepto版本和jquery版本分开

--- gruntfile.js
+++ gruntfile.js
@@ -15,16 +15,17 @@ module.exports = function (grunt) {
 		        banner: '/*! \n版本:<%= pkg.version %>;\n作者:<%= pkg.author.name %>;\n邮箱:<%= pkg.author.email %>;\n博客地址:<%= pkg.author.blog %>;*/\n'
 		      },
 			my_target: {
-				files:{
-		        		'dist/js/mvalidate.js': ['demo/js/mvalidate.js']
-		      		}
+				files:[
+		        		{'dist/js/jquery-mvalidate.js': ['demo/js/jquery-mvalidate.js']},
+		        		{'dist/js/zepto-mvalidate.js': ['demo/js/zepto-mvalidate.js']}
+		      		]
 	      	}
 		}, 
 		copy:{
 			my_target: {
 				files:[
 					{expand:true,src:["demo/css/validate.css"],dest:"src/css",flatten:true},
-					{expand:true,src:["demo/js/mvalidate.js"],dest:"src/js",flatten:true}
+					{expand:true,src:["demo/js/*.js"],dest:"src/js",flatten:true}
 				]
 			}
 		}
